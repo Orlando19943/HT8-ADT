60963d1aa6705d67b3f97a4787243f8ec0be7ece
Orlando Cabrera <orlando.osberto@gmail.com>
2020-03-26 11:45:18 -0600

    Le puse que leyera el archivo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,6 +17,27 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner (System.in);
+		int tipo = 0;	
+		System.out.println("Introduzca el nombre del documento (cards_desc.txt)");
+		String documento;
+		teclado.nextLine();
+		documento = teclado.nextLine();
+		String texto = "";
+	    try {
+	      FileReader fr = new FileReader(documento);
+	      BufferedReader br = new BufferedReader(fr);
+	      String linea;	      
+	      while((linea = br.readLine()) != null) {
+	    	  texto = texto + linea;    	  
+	      }
+	    	  
+	        
+	      fr.close();
+	    }
+	    catch(Exception e) {
+	      System.out.println("Excepcion leyendo fichero "+ documento + ": " + e);
+	    }
 
 	}
 
